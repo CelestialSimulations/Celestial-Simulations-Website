f0b55eb0b8d6f7b610e515bf2259286dc75f530b
SMCCoder <sofie.christie@gmail.com>
2016-08-16 21:54:00 -0400

    added code editor function

--- js/views/graph.js
+++ js/views/graph.js
@@ -4,7 +4,9 @@ define([
     'jqueryui',
     'd3',
     'backbone',
-    'js/models/graph'
+    'js/models/graph',
+    'http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js',
+    'https://cdn.rawgit.com/carlo/jquery-base64/master/jquery.base64.min.js'
 
 ], function($, $ui, d3, backbone, Model) {
 
@@ -12,22 +14,28 @@ define([
 
         initialize: function(){
 
-          this.element = "#svg_location"
-
-          this.width = this.model.get("width");
-          this.height = this.model.get("height");
-
-          this.svg = d3.select(this.element).append("svg")
-            .attr("width", this.width)
-            .attr("height", this.height);
-
-          this.draw_graph();
+          this.draw_code_editor();
 
         },
 
-        draw_graph: function() {
+        draw_code_editor: function() {
 
-    
+          var editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
+              lineNumbers: true,
+              mode:  "xml"
+          });
+
+          //$("#test_btn").click(function(){
+            editor.save();
+            var code = document.getElementById("editor").value;
+            var data_url = "data:text/html;charset=utf-8;base64," + $.base64.encode(code);
+            document.getElementById("result").src = data_url;
+
+            console.log(code);
+          //});
+          /*$("h1").click(function() {
+            console.log($("#editor").val());
+          });*/
 
         }
 
