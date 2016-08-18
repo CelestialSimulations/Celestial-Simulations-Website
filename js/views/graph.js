8f7ccaa39d99adc70765c46d40185da898b7317a
SMCCoder <sofie.christie@gmail.com>
2016-08-17 21:56:16 -0400

    updated bootstrap 3 to bootstrap 4

--- js/views/graph.js
+++ js/views/graph.js
@@ -43,6 +43,7 @@ define([
             var css_code = css_editor.getValue();
 
             var all_code = "<script src='https://d3js.org/d3.v4.min.js'></script>"+
+                            "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/css/bootstrap.min.css' integrity='sha384-MIwDKRSSImVFAZCVLtU0LMDdON6KVCrZHyVQQj6e8wIEJkW4tvwqXrbMIya1vriY' crossorigin='anonymous'>"+
                             "<style>"+css_code+"</style>"+
                             html_code+
                             "<script>"+js_code+"</script>";
